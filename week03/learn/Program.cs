750db0f94f28a599cabf534dd3b0f0e74aa0b5da
reginmund9 <var23004@byui.edu>
2024-03-14 23:25:08 -0600

    w3

--- week03/learn/Program.cs
+++ week03/learn/Program.cs
@@ -5,7 +5,7 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("\n======================\nDuplicate Counter\n======================");
-        DuplicateCounter.Run();
+        DuplicateCounterSolution.Run();
 
         Console.WriteLine("\n======================\nTranslator\n======================");
         TranslatorSolution.Run();
